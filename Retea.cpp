56a13f9415a9072d94be049f476df90bb3ef1739
Iulian Chivu <iulianchivu98@yahoo.com>
2020-04-15 12:16:53 +0300

    Update Retea.cpp

--- Retea.cpp
+++ Retea.cpp
@@ -1,97 +1,97 @@
 #include<iostream>
+#include<math.h>
 
 using namespace std;
 
 class Nod{
 	
-
 	public:
 	float w, b;
-	Nod *next; //pointer spre urmatorul layer de noduri
-	Nod *prev; //pointer spre stratul anterior(Pentru backwards prop)
+	Nod *next; 
 
 	Nod(){
 		w=0.93; //o sa le initializez random
 		b=0;
 		next=NULL;
-		prev=NULL;
 	}
-
+	
+	float sigmoid(float x){
+		return (float)1 / (1 + exp(-x));
+	}
 };
 
 class Retea{
-	int n; //nr straturi ascunse
-	int *v; //vector cu dim fiecarui strat de noduri
-	Nod *start; //primul nod din retea
 	
 	public:
+	Nod *first_nod;
+	Retea *next;
+	Retea *prev; //pentru backwards prop
+	
 	Retea(){
-		cout<<"Introduceti nr. de straturi ascunse: ";
-		cin>>n;
-		v = new int[n+1];
-
-		for(int i=0;i<n;i++){
-			cout<<"Introduceti nr. de noduri pentru stratul "<<i+1<<": ";
-			cin>>v[i];
-		}
-		cout<<"Introduceti nr. nodurilor de iesire: ";
-		cin>>v[n];
-		
-		cout<<"\nStructura retea: strat intrare -> ";
-		for(int i=0;i<n+1;i++){
-			cout<<v[i]<<" -> ";
-		}
-		cout<<"Iesire"<<endl;
-		
-		start = NULL;
+		first_nod = NULL;
+		next = NULL;
+		prev = NULL;
 	}
-
-	void initializare(){
-		Nod *nod_nou, *tmp;
+	
+	Retea *initializare(int &v, int n, Retea *prim){
+		Retea *layer_nou;
+		Retea *tmp;
+		Nod *nod_nou;
+		Nod *nod_tmp;
 		for(int i=0;i<n+1;i++){
-			nod_nou = new Nod[v[i]];
-			if(start == NULL) start = nod_nou;
-			else{
-				tmp = start;
-				while(tmp->next!=NULL) tmp=tmp->next;
-				tmp->next=nod_nou;
-				nod_nou->prev=tmp;
-			}
-		}
-	}
-
-	void verificare_afisare(){
-		Nod *tmp=start;
-		while(tmp!=NULL){
-			for(int i=0;i<n+1;i++){		
-			cout<<"ponderi stratul nr "<<i+1<<": ";	
-				for(int j=0;j<v[i];j++){
-					cout<<tmp[j].w<<" ";
+			layer_nou = new Retea;
+			
+			for(int j=0;j<*(&v+i);j++){
+				nod_nou = new Nod;
+				if(first_nod == NULL){
+					first_nod = nod_nou;
 				}
-				tmp=tmp->next;
-				cout<<endl;
+				else{
+					nod_tmp = first_nod;
+					while(nod_tmp->next!=NULL) nod_tmp=nod_tmp->next;
+					nod_tmp->next=nod_nou;
+				}
+			}
+			
+			if(prim == NULL) {
+				prim = layer_nou;
+			}
+			else{
+				tmp = prim;
+				while(tmp->next!=NULL) tmp=tmp->next;
+				tmp->next=layer_nou;
+				layer_nou->prev=tmp;
 			}
 		}
-	}
-
-	~Retea(){
-		delete []v;
+		return prim;
 	}
 };
 
-
-
-
-
 int main(){
 	
-	cout<<endl;
-	Retea r;
-	r.initializare();
-	cout<<endl;
-	r.verificare_afisare();
+	int n;
+	int *v;
+	
+	cout<<"Introduceti nr. de straturi ascunse: ";
+	cin>>n;
+	v = new int[n+1];
+
+	for(int i=0;i<n;i++){
+		cout<<"Introduceti nr. de noduri pentru stratul "<<i+1<<": ";
+		cin>>v[i];
+	}
+	cout<<"Introduceti nr. nodurilor de iesire: ";
+	cin>>v[n];
+		
+	cout<<"\nStructura retea: intrare -> ";
+	for(int i=0;i<n+1;i++){
+		cout<<v[i]<<" -> ";
+	}
+	cout<<"Iesire"<<endl;
+	
+	Retea *r = NULL;
+	r = r->initializare(*v,n,r);
+	
 
 	return 0;
 }
-
-
