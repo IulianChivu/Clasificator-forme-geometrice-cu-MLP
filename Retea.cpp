bbb558357321e2673b30d6583efe3b4fe7bbd052
Iulian Chivu <iulianchivu98@yahoo.com>
2020-05-09 20:12:23 +0300

    Incercare etapa Forward

--- Retea.cpp
+++ Retea.cpp
@@ -1,97 +1,181 @@
 #include<iostream>
-#include<math.h>
-
 using namespace std;
+int *dim = NULL; //vector care contine dimensiunile fiecarui strat + dimensiunea vectorului de intrare
+int c = 0; //contor pentru vectorul de dimensiuni
 
 class Nod{
+	float w;
+	Nod *next;
 	
 	public:
-	float w, b;
-	Nod *next; 
-
-	Nod(){
-		w=0.93; //o sa le initializez random
-		b=0;
-		next=NULL;
-	}
-	
-	float sigmoid(float x){
-		return (float)1 / (1 + exp(-x));
-	}
+		Nod(){
+			w = 0;
+			next = NULL;
+		}
+		
+		Nod *creare(Nod *prim){
+			Nod *nou, *tmp;
+			int n;
+			if(prim!=NULL){
+				cout<<"Lista de noduri nu este vida!"<<endl;
+				return prim;
+			}
+			cout<<"introduceti numarul de noduri dorit pe acest strat: ";
+			cin>>n;
+			
+			for(int i=0;i<n;i++){
+				//creare si citirea unui nod individual:
+				nou = new Nod;
+				if(!nou){
+					cout<<"Memoria nu a putut fi alocata"<<endl;
+					return NULL;
+				}
+				cout<<"Valoarea pentru nodul nr "<<i+1<<":";
+				cin>>nou->w;
+				nou->next=NULL;
+				//caz particular pentru primul nod:
+				if(prim==NULL){
+					prim = nou;
+				}
+				//in cazul celorlalte noduri:
+				else{
+					tmp = prim;
+					while(tmp->next!=NULL) tmp=tmp->next;
+					tmp->next=nou;
+				}
+			}
+			return prim;
+		}
+		
+		void afisare(Nod *prim){
+			Nod *tmp;
+			tmp = prim;
+			if(prim==NULL){
+				cout<<"Lista de noduri este goala!"<<endl;
+			}
+			else{
+				while(tmp!=NULL){
+					cout<<tmp->w<<" -> ";
+					tmp=tmp->next;
+				}
+				cout<<"NULL"<<endl;
+			}
+		}
+		
+		Nod *stergere(Nod *prim){
+			Nod *tmp;
+			tmp = prim;
+			if(prim==NULL){
+				cout<<"Lista de noduri este vida!"<<endl;
+				return NULL;
+			}
+			while(tmp!=NULL){
+				tmp=tmp->next;
+				delete prim;
+				prim = tmp;
+			}
+			cout<<"Lista de noduri a fost stearsa!"<<endl;
+			return NULL;
+		}
 };
 
 class Retea{
+	Nod *primul_nod; //primul nod din fiecare strat
+	Retea *next;
 	
 	public:
-	Nod *first_nod;
-	Retea *next;
-	Retea *prev; //pentru backwards prop
-	
-	Retea(){
-		first_nod = NULL;
-		next = NULL;
-		prev = NULL;
-	}
-	
-	Retea *initializare(int &v, int n, Retea *prim){
-		Retea *layer_nou;
-		Retea *tmp;
-		Nod *nod_nou;
-		Nod *nod_tmp;
-		for(int i=0;i<n+1;i++){
-			layer_nou = new Retea;
-			
-			for(int j=0;j<*(&v+i);j++){
-				nod_nou = new Nod;
-				if(first_nod == NULL){
-					first_nod = nod_nou;
-				}
-				else{
-					nod_tmp = first_nod;
-					while(nod_tmp->next!=NULL) nod_tmp=nod_tmp->next;
-					nod_tmp->next=nod_nou;
-				}
+		Retea(){
+			primul_nod=NULL;
+			next=NULL;
+		}
+		
+		Retea *creare(Retea *prim){
+			Retea *nou, *tmp;
+			int n;
+			if(prim!=NULL){
+				cout<<"Lista de straturi nu este vida!"<<endl;
+				return prim;
 			}
 			
-			if(prim == NULL) {
-				prim = layer_nou;
+			cout<<"Introduceti numarul de straturi (straturi ascunse + strat de iesire): ";
+			cin>>n;
+			dim = new int[n+1];  
+			
+			for(int i=0;i<n;i++){
+				//creare si citirea unui nod individual:
+				nou = new Retea;
+				if(!nou){
+					cout<<"Memoria nu a putut fi alocata"<<endl;
+					return NULL;
+				}
+				if(i+1==n) cout<<"Stratul nr "<<i+1<<" (stratul de iesire): ";
+				else cout<<"Stratul nr "<<i+1<<": ";
+				nou->primul_nod = nou->primul_nod->creare(nou->primul_nod); //initializarea nodurilor de pe strtul curent
+				nou->next=NULL;
+				//caz particular pentru primul nod:
+				if(prim==NULL){
+					prim = nou;
+				}
+				//in cazul celorlalte noduri:
+				else{
+					tmp = prim;
+					while(tmp->next!=NULL) tmp=tmp->next;
+					tmp->next=nou;
+				}
+			}
+			return prim;
+		}
+		
+		void afisare(Retea *prim){
+			Retea *tmp;
+			tmp = prim;
+			if(prim==NULL){
+				cout<<"Lista de straturi este goala!"<<endl;
 			}
 			else{
-				tmp = prim;
-				while(tmp->next!=NULL) tmp=tmp->next;
-				tmp->next=layer_nou;
-				layer_nou->prev=tmp;
+				cout<<"Intrare"<<endl;
+				cout<<"   |   "<<endl;
+				while(tmp!=NULL){
+					tmp->primul_nod->afisare(tmp->primul_nod);
+					cout<<"   |   "<<endl;
+					tmp=tmp->next;
+				}
+				cout<<" NULL"<<endl;
 			}
 		}
-		return prim;
-	}
+		
+		Retea *stergere(Retea *prim){
+			Retea *tmp;
+			tmp = prim;
+			if(prim==NULL){
+				cout<<"Lista de straturi este vida!"<<endl;
+				return NULL;
+			}
+			while(tmp!=NULL){
+				tmp=tmp->next;
+				prim->primul_nod = prim->primul_nod->stergere(prim->primul_nod);
+				delete prim;
+				prim = tmp;
+			}
+			cout<<"Lista de straturi a fost stearsa!"<<endl;
+			return NULL;
+		}
+		
 };
+
 
 int main(){
 	
-	int n;
-	int *v;
+	Retea *prim = NULL;
 	
-	cout<<"Introduceti nr. de straturi ascunse: ";
-	cin>>n;
-	v = new int[n+1];
-
-	for(int i=0;i<n;i++){
-		cout<<"Introduceti nr. de noduri pentru stratul "<<i+1<<": ";
-		cin>>v[i];
-	}
-	cout<<"Introduceti nr. nodurilor de iesire: ";
-	cin>>v[n];
-		
-	cout<<"\nStructura retea: intrare -> ";
-	for(int i=0;i<n+1;i++){
-		cout<<v[i]<<" -> ";
-	}
-	cout<<"Iesire"<<endl;
+	prim = prim->creare(prim);
+	cout<<endl;
+	prim->afisare(prim);
+	cout<<endl;
 	
-	Retea *r = NULL;
-	r = r->initializare(*v,n,r);
+	prim = prim->stergere(prim);
+	prim->afisare(prim);
 	
-
 	return 0;
 }
+
